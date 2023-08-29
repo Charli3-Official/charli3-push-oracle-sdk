942cdf63e614cd84da1edd4e82d6ef9a9d935082
José Seraquive <jose.seraquive@gmail.com>
2023-08-29 12:00:49 -0500

    Feature/fix plutip tests#68 (#70)
    - Removed os.rmdir("binary"), it generated an error when creating a new instance of the contract
    - Updated binary (Updated oracle test address)
    - Correction for relative file paths
    - Decreased test nodes from 5 to 3
    - Added verification for update-node transaction (integration-tests)
    - Added missing shared C library (libsecp256k1 )
    - Fixed minimum top-up amount

--- integration-tests/run_integration_tests.sh
+++ integration-tests/run_integration_tests.sh
@@ -21,21 +21,21 @@ kill_processes() {
 trap 'kill_processes' SIGINT SIGTERM
 
 # Remove content of directories if they exist
-if [ -d "./wallets" ]; then
-    rm -rf ./wallets/*
+if [ -d "$PWD/wallets" ]; then
+    rm -rf "$PWD"/wallets/*
 fi
-if [ -d "./db" ]; then
-    rm -rf ./db/*
+if [ -d "$PWD/db" ]; then
+    rm -rf "$PWD"/db/*
 fi
 
 # Create directories if they don't exist
-mkdir -p ./wallets
-mkdir -p ./db
+mkdir -p "$PWD"/wallets
+mkdir -p "$PWD"/db
 
 # Start the local cluster and dump the output to JSON
 local-cluster -n 9 \
-    --dump-info-json ./local-cluster-info.json \
-    -d ./wallets/ \
+    --dump-info-json "$PWD"/local-cluster-info.json \
+    -d "$PWD"/wallets/ \
     --ada 200 \
     --utxos 5  \
     -s 1s -e 3600 \
@@ -47,10 +47,15 @@ local_cluster_pid=$!
 echo "Running local-cluster ..."
 
 # Give it some time to start up
-sleep 5
+sleep 10
+
+if [ ! -f "$PWD/local-cluster-info.json" ]; then
+    echo "Error: JSON file does not exist"
+    exit 1
+fi
 
 # Extract the temporal directory from the JSON file using jq (a command-line JSON processor)
-temp_dir=$(dirname $(jq -r '.ciNodeSocket' ./local-cluster-info.json))
+temp_dir=$(dirname $(jq -r '.ciNodeSocket' "$PWD/local-cluster-info.json"))
 
 # Run the other commands with the extracted directory
 ogmios --node-socket $temp_dir/node.socket \
