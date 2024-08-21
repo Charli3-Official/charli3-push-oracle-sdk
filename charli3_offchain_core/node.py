63bd0d58fb7f9a3556301b083521fb95711a6708
xxAVOGADROxx <jose.seraquive@gmail.com>
2024-08-20 22:13:46 -0500

    Added `fee_buffer` in all txs

--- charli3_offchain_core/node.py
+++ charli3_offchain_core/node.py
@@ -115,7 +115,7 @@ class Node:
 
             node_update_redeemer = Redeemer(NodeUpdate())
 
-            builder = TransactionBuilder(self.context)
+            builder = TransactionBuilder(self.context, fee_buffer=250000)
 
             script_utxo = (
                 await self.chain_query.get_reference_script_utxo(
@@ -251,7 +251,7 @@ class Node:
                     else None
                 )
 
-                builder = TransactionBuilder(self.context)
+                builder = TransactionBuilder(self.context, fee_buffer=500000)
 
                 aggstate_tx_output = deepcopy(aggstate_utxo.output)
                 aggstate_tx_output.amount.multi_asset[self.c3_token_hash][
@@ -414,7 +414,7 @@ class Node:
             else None
         )
 
-        builder = TransactionBuilder(self.context)
+        builder = TransactionBuilder(self.context, fee_buffer=250000)
 
         (
             builder.add_script_input(
